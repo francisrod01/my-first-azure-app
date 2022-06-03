1963ffc11b58cb384cc758ebef94655d3d9ede3d
Francis Rodrigues <francis.rod01@outlook.com>
2022-06-02 23:29:07 -0300

    feat(test front-end): Added http client testing module

--- front-end/src/app/azure/components/list-cosmos/list-cosmos.component.spec.ts
+++ front-end/src/app/azure/components/list-cosmos/list-cosmos.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ListCosmosComponent } from './list-cosmos.component';
 
@@ -8,7 +9,8 @@ describe('ListCosmosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListCosmosComponent ]
+      declarations: [ ListCosmosComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
--- front-end/src/app/azure/services/cosmos/cosmos.service.spec.ts
+++ front-end/src/app/azure/services/cosmos/cosmos.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CosmosService } from './cosmos.service';
 
@@ -6,7 +7,9 @@ describe('CosmosService', () => {
   let service: CosmosService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(CosmosService);
   });
 
